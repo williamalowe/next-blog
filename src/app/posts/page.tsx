a9b046ca2be74ad5f91a0eaf66f5fa4a2418ef21
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-01 13:14:58 +1000

    refactor: add posts list loading suspense

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,26 +1,16 @@
+import PostsList from "@/components/posts-list";
 import Link from "next/link";
+import { Suspense } from "react";
 
 export default async function Posts() {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  const response = await fetch('https://dummyjson.com/posts?limit=10');
-  const data = await response.json();
-
   return (
     <main className="text-center pt-16 px-5">
       <h1 className="text-4xl font-bold mb-5 md:text-5xl">
         All Posts
       </h1>
-     <ul>
-      {
-        data.posts.map((post) => 
-        <li key={post.id} className="mb-3">
-          <Link href={`/posts/${post.id}`}>{post.title}</Link>
-          {/* <p className="text-lg">{post.body}</p> */}
-        </li>
-        )
-      }
-     </ul>
+      <Suspense fallback="loading...">
+        <PostsList />
+      </Suspense>
     </main>
   );
 }
