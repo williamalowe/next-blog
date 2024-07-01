7dbfb46a14987fafc99687b4c649d934323749d8
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-01 13:26:27 +1000

    feat: add upvote button-component

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,3 +1,5 @@
+import UpvoteBtn from "@/components/upvote-btn";
+
 export default async function Page({ params } : {
   params: {id: string}
 }) {
@@ -8,6 +10,8 @@ export default async function Page({ params } : {
     <main className="px-7 pt-24 text-center">
       <h1 className="text-5xl font-semibold mb-7">{post.title}</h1>
       <p className="max-w-[700px] mx-auto">{post.body}</p>
+
+      <UpvoteBtn />
     </main>
   )
 }
---
+++ src/components/upvote-btn.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { useState } from "react";
+
+export default function UpvoteBtn() {
+  const [upvoteCount, setUpvoteCount] = useState(0);
+  return (
+    <button
+      onClick={() => setUpvoteCount(upvoteCount + 1)}
+      className="bg-blue-500 text-white p-2 mt-10"
+    >
+      Upvote {upvoteCount}
+    </button>
+  );
+}
